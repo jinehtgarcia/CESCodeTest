4b24a087a8d52b5d525757a21fc5bd6e9ca0854f
Jineht Garcia <jineht.garcia@gmail.com>
2017-05-21 10:49:10 -0400

    Adding controller and model

---
+++ application/models/User_model.php
@@ -0,0 +1,67 @@
+<?php
+class User_model extends CI_Model {
+
+	public function __construct() {
+		$this->load->database();
+	}
+	
+	public function get_user($id = FALSE) {
+		if ($id === FALSE)
+		{
+			$query = $this->db->get('user');
+			return $query->result_array();
+		}
+	
+		$query = $this->db->get_where('user', array('id' => $id));
+		return $query->row_array();
+	}
+	
+	public function delete_user($id) {
+		$this->db->delete('user', array('id' => $id)); 
+	}
+
+	public function exist_email($email = FALSE) {
+		$this->db->select('id');
+		$this->db->from('user');
+		$this->db->where('email', $email);
+		$result = $this->db->get()->result_array()[0]['id'];
+		return $result;
+	}
+	
+	public function empty_user() {
+		$user['name'] = '';
+		$user['birthdate'] = '';
+		$user['email'] = '';
+		$user['favorite_color'] = '';
+		return $user;		
+	}
+	
+	public function set_user() {
+    	$this->load->helper('url');
+
+	    $data = array(
+	        'name' => $this->input->post('name'),
+	    	'email' => $this->input->post('email'),
+	    	'birthdate' => $this->input->post('birthdate'),
+	    	'favorite_color' => $this->input->post('favcolor')
+	    );
+
+    	return $this->db->insert('user', $data);
+	}
+	
+
+	public function edit_user() {
+		$this->load->helper('url');
+	
+		$data = array(
+				'id'  => $this->input->post('id'),
+				'name' => $this->input->post('name'),
+				'email' => $this->input->post('email'),
+				'birthdate' => $this->input->post('birthdate'),
+				'favorite_color' => $this->input->post('favcolor')
+		);
+	
+		$this->db->where('id', $this->input->post('id'));
+		return $this->db->update('user', $data);
+	}
+}
